a688db29aa61095a76c6d1fa90dee36115a1381c
Francesco-Munafo <acquamarea1@gmail.com>
2023-11-07 12:19:17 +0100

    Add contacts and about pages w/ routes

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-6 text-center">
+                <h1>Contacts</h1>
+            </div>
+
+        </div>
+    </div>
+@endsection
